2a0a628afc1d3ea61c62b7567f9f21fdd3c1c4d6
rezanachmad <rezanachmad@22032971-f4d5-6d47-c384-d3e7bb77ae29>
2011-04-30 13:07:48 +0000

    Server

--- trunk/src/KotakServer/src/kotakserver/Main.java
+++ trunk/src/KotakServer/src/kotakserver/Main.java
@@ -5,6 +5,12 @@
 
 package kotakserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author user
@@ -15,7 +21,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int port = 7000;
+
+        try {
+            ServerSocket ss = new ServerSocket(port);
+
+            // Loop forever until die
+            while (true) {
+                // Waiting for connection
+                Socket socket = ss.accept();
+
+                // There is a connection, serve it!
+                (new Server(socket)).start();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
